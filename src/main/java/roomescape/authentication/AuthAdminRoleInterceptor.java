347d0c7a8539a6ccfd4945550078d650d45304c0
goldm0ng <jujs45177@gmail.com>
2024-12-26 14:48:15 +0900

    <ADD> 관리자 기능 구현

---
+++ src/main/java/roomescape/authentication/AuthAdminRoleInterceptor.java
@@ -0,0 +1,33 @@
+package roomescape.authentication;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import roomescape.jwt.JwtProvider;
+import roomescape.jwt.JwtResponse;
+import roomescape.jwt.JwtUtils;
+
+@Component
+@RequiredArgsConstructor
+public class AuthAdminRoleInterceptor implements HandlerInterceptor {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        JwtResponse jwtResponse = JwtUtils.extractTokenFromCookie(request.getCookies());
+        if (jwtResponse.accessToken() == null || !isAdmin(jwtResponse.accessToken())) {
+            response.setStatus(401);
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isAdmin(String token) {
+        MemberAuthInfo memberAuthInfo = JwtUtils.extractMemberAuthInfoFromToken(token);
+        return "ADMIN".equals(memberAuthInfo.role());
+    }
+}
