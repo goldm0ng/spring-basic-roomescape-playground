e027b31d3e86d0fea0c836b619d6dbb87f552364
goldm0ng <jujs45177@gmail.com>
2025-01-08 02:13:44 +0900

    <FIX> 응답 바디 타입 수정

--- src/main/java/roomescape/login/LoginController.java
+++ src/main/java/roomescape/login/LoginController.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,34 +20,30 @@ public class LoginController {
     private final LoginService loginService;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+    public void login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse response) {
         JwtResponse jwtResponse = loginService.login(loginRequest);
 
         Cookie cookie = new Cookie("token", jwtResponse.accessToken());
         cookie.setHttpOnly(true);
         cookie.setPath("/");
         response.addCookie(cookie);
-
-        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/login/check")
-    public ResponseEntity<LoginCheckResponse> checkLogin(HttpServletRequest request) {
+    public LoginCheckResponse checkLogin(HttpServletRequest request) {
         JwtResponse jwtResponse = JwtUtils.extractTokenFromCookie(request.getCookies());
         MemberAuthInfo memberAuthInfo = JwtUtils.extractMemberAuthInfoFromToken(jwtResponse.accessToken());
         LoginCheckResponse loginCheckResponse = loginService.checkLogin(memberAuthInfo);
 
-        return ResponseEntity.ok(loginCheckResponse);
+        return loginCheckResponse;
     }
 
     @PostMapping("/logout")
-    public ResponseEntity logout(HttpServletResponse response) {
+    public void logout(HttpServletResponse response) {
         Cookie cookie = new Cookie("token", "");
         cookie.setHttpOnly(true);
         cookie.setPath("/");
         cookie.setMaxAge(0);
         response.addCookie(cookie);
-
-        return ResponseEntity.ok().build();
     }
 }
