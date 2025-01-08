9c5e253f190e565f1b6a4bfa5f475e7b704b7e47
goldm0ng <jujs45177@gmail.com>
2025-01-08 23:19:56 +0900

    <FIX> Spring MVC (인증) 미션 테스트 이름 수정

--- src/test/java/roomescape/SpringMVCTest.java
+++ src/test/java/roomescape/SpringMVCTest.java
@@ -16,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+public class SpringMVCTest {
 
     private String createToken(String email, String password) {
         Map<String, String> params = new HashMap<>();
@@ -104,4 +104,5 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200);
     }
+
 }
