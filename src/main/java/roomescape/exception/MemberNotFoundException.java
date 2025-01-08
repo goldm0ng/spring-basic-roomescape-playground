f164ce0686a60976349fe3fe4b54785e8fa9821b
goldm0ng <jujs45177@gmail.com>
2025-01-08 23:18:10 +0900

    <FIX> JPA 전환에 따른 LoginService 수정 및 예외처리 방식 변경

--- src/main/java/roomescape/exception/MemberNotFoundException.java
+++ src/main/java/roomescape/exception/MemberNotFoundException.java
@@ -1,6 +1,11 @@
 package roomescape.exception;
 
 public class MemberNotFoundException extends RuntimeException {
+
+    public MemberNotFoundException(String message) {
+        super(message);
+    }
+
     public MemberNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
--- src/main/java/roomescape/login/LoginService.java
+++ src/main/java/roomescape/login/LoginService.java
@@ -1,39 +1,34 @@
 package roomescape.login;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import roomescape.authentication.MemberAuthInfo;
 import roomescape.authentication.jwt.JwtResponse;
 import roomescape.authentication.jwt.JwtUtils;
 import roomescape.member.Member;
-import roomescape.member.MemberDao;
 import roomescape.exception.MemberNotFoundException;
+import roomescape.member.MemberRepository;
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
 
-    private final MemberDao memberDao;
+    private final MemberRepository memberRepository;
     private final JwtUtils jwtUtils;
 
     public JwtResponse login(LoginRequest loginRequest) {
 
-        try {
-            Member member = memberDao.findByEmailAndPassword(loginRequest.email(), loginRequest.password());
-            return jwtUtils.createAccessToken(member);
-        } catch (EmptyResultDataAccessException e) {
-            throw new MemberNotFoundException("이메일 혹은 비밀번호가 맞지 않습니다.", e);
-        }
+        Member member = memberRepository.findByEmailAndPassword(loginRequest.email(), loginRequest.password())
+                .orElseThrow(() -> new MemberNotFoundException("입력한 이메일 혹은 비밀번호로 가입한 회원을 찾을 수 없습니다."));
+
+        return jwtUtils.createAccessToken(member);
     }
 
     public LoginCheckResponse checkLogin(MemberAuthInfo memberAuthInfo) {
 
-        try {
-            Member member = memberDao.findByName(memberAuthInfo.name());
-            return new LoginCheckResponse(member.getName());
-        } catch (EmptyResultDataAccessException e) {
-            throw new MemberNotFoundException("로그인이 되지 않은 상태입니다.", e);
-        }
+        Member member= memberRepository.findByName(memberAuthInfo.name())
+                .orElseThrow(() -> new MemberNotFoundException("로그인 된 회원이 아닙니다."));
+
+        return new LoginCheckResponse(member.getName());
     }
 }
