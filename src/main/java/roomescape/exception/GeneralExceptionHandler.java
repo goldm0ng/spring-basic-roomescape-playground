e7e5c7b17f9da029fa285411b45efc486e93a984
goldm0ng <jujs45177@gmail.com>
2025-01-13 22:10:10 +0900

    <FIX> 공통 예외 처리 핸들러 중복 코드 제거

--- src/main/java/roomescape/exception/GeneralExceptionHandler.java
+++ src/main/java/roomescape/exception/GeneralExceptionHandler.java
@@ -1,46 +1,33 @@
 package roomescape.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GeneralExceptionHandler {
+
+    @ExceptionHandler({MemberNotFoundException.class, JwtValidationException.class, JwtProviderException.class})
+    public ResponseEntity<String> handleMemberNotFound(MemberNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+
+    @ExceptionHandler({TimeNotFoundException.class, ThemeNotFoundException.class})
+    public ResponseEntity<String> handleTimeNotFound(TimeNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
 
     @ExceptionHandler(DuplicateReservationException.class)
     public ResponseEntity<String> handleDuplicatedReservation(DuplicateReservationException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
     }
 
-    @ExceptionHandler(TimeNotFoundException.class)
-    public ResponseEntity<String> handleTimeNotFound(TimeNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-    }
-
-    @ExceptionHandler(ThemeNotFoundException.class)
-    public ResponseEntity<String> handleThemeNotFound(ThemeNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-    }
-
-    @ExceptionHandler(MemberNotFoundException.class)
-    public ResponseEntity<String> handleMemberNotFound(MemberNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
-    }
-
-    @ExceptionHandler(JwtValidationException.class)
-    public ResponseEntity<String> handleJwtValidationException(JwtValidationException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
-    }
-
-    @ExceptionHandler(JwtProviderException.class)
-    public ResponseEntity<String> handleJwtProviderException(JwtProviderException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
-    }
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception e) {
-        e.printStackTrace();
+        log.error("Exception [Err_Mas]: {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }
