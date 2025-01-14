08017a749d947f44404769043c8f61bc218ad4c5
goldm0ng <jujs45177@gmail.com>
2025-01-15 01:10:40 +0900

    <FIX> JPA 쿼리 메소드 적용

--- src/main/java/roomescape/reservation/ReservationRepository.java
+++ src/main/java/roomescape/reservation/ReservationRepository.java
@@ -1,8 +1,6 @@
 package roomescape.reservation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,8 +10,5 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     List<Reservation> findByName(String name);
 
-    @Query("SELECT COUNT(r) > 0 FROM Reservation r WHERE r.date = :date AND r.time.id = :timeId AND r.theme.id = :themeId")
-    boolean existsByDateAndTimeIdAndThemeId(@Param("date") String date,
-                                            @Param("timeId") Long timeId,
-                                            @Param("themeId") Long themeId);
+    boolean existsByDateAndTimeIdAndThemeId(String date, Long timeId, Long themeId);
 }
