ed9cc5d12e636f8806ed8949ea76b33fd1b8a55b
goldm0ng <jujs45177@gmail.com>
2025-01-15 11:45:31 +0900

    <ADD> 인증 로직을 담은 빈이 수동 주입되도록 관리하는 AuthenticationConfig 추가

---
+++ src/main/java/roomescape/authentication/AuthenticationConfig.java
@@ -0,0 +1,20 @@
+package roomescape.authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import roomescape.authentication.jwt.JwtAuthenticationInfoExtractor;
+import roomescape.authentication.jwt.JwtAuthenticationProvider;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticationConfig {
+
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final JwtAuthenticationInfoExtractor jwtAuthenticationInfoExtractor;
+
+    @Bean
+    public AuthenticationService authenticationService(){
+        return new AuthenticationService(jwtAuthenticationProvider, jwtAuthenticationInfoExtractor);
+    }
+}
