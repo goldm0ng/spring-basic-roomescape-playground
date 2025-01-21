3ddd7e9c527f9035fb7259edbcdcc98543cf676d
goldm0ng <jujs45177@gmail.com>
2025-01-21 21:33:54 +0900

    <ADD> 에러 응답 객체 생성 및 예외 응답 구성 변경

--- src/main/java/roomescape/exception/GeneralExceptionHandler.java
+++ src/main/java/roomescape/exception/GeneralExceptionHandler.java
@@ -11,23 +11,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GeneralExceptionHandler {
 
     @ExceptionHandler({MemberNotFoundException.class, JwtValidationException.class, JwtProviderException.class})
-    public ResponseEntity<String> handleMemberNotFound(Exception e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleMemberNotFound(Exception e) {
+        ErrorResponse authenticationErrorResponse = new ErrorResponse(e.getMessage(), "authentication_error");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(authenticationErrorResponse);
     }
 
     @ExceptionHandler({TimeNotFoundException.class, ThemeNotFoundException.class})
-    public ResponseEntity<String> handleTimeNotFound(Exception e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleTimeNotFound(Exception e) {
+        ErrorResponse notFoundErrorResponse = new ErrorResponse(e.getMessage(), "not_found");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(notFoundErrorResponse);
     }
 
     @ExceptionHandler(DuplicateReservationException.class)
-    public ResponseEntity<String> handleDuplicatedReservation(DuplicateReservationException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleDuplicatedReservation(DuplicateReservationException e) {
+        ErrorResponse duplicationErrorResponse = new ErrorResponse(e.getMessage(), "duplicate_reservation");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(duplicationErrorResponse);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGeneralException(Exception e) {
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception e) {
         log.error("Exception [Err_Location] : {}", e.getStackTrace()[0], e);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("잠깐 문제가 생겼어요. 다음에 다시 시도해주세요.");
+        ErrorResponse errorResponse = new ErrorResponse("잠깐 문제가 생겼어요. 다음에 다시 시도해주세요.", "internal_server_error");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 }
